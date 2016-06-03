4032b251d99cdd1b769b216944c9bbb34931282d
Rafael Almeida <rafaelcpalmeida@gmail.com>
2016-06-03 18:09:21 +0100

    Initial testing

---
+++ testing.js
@@ -0,0 +1,8 @@
+//alert(Math.floor(Math.random() * 6) + 1);
+var t = new AVLTree();
+var a = t.insert(1, 'hello');
+var b = t.insert(2, 'world');
+t.search(1); // returns a
+console.log(t.search(3)); // returns b, the last leaf visited while searching for 3
+console.log(dicewareWords);
+console.log(dicewareWords["11111"]);
