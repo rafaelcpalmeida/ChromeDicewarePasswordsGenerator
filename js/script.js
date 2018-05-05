069d015e69c70efea118ee53872e21d3945bcfb0
Nuno Azevedo <nunoemanuel1@gmail.com>
2018-05-05 03:25:15 +0100

    Typeface changed; Add Dark Theme; URL's fixed;  TextArea changed to read-only

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var newPasswordButton = document.getElementById("newPassword");
 var generatedPasswordText = document.getElementById("generatedPassword");
 var numberOfWords = document.getElementById("numberOfWords");
 var separatorValue = document.getElementById("separatorValue");
+var light = document.getElementById("light");
+var darkside = document.getElementById("darkside");
+
 
 newPasswordButton.onclick = function() {
     let dicewareWords = document.querySelector('input[type="checkbox"]');
@@ -14,4 +17,26 @@ newPasswordButton.onclick = function() {
         generatedPasswordText.textContent = response.password;
         generatedPasswordText.select();
     });
+};
+
+light.onclick = function() {
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = 'light';
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'css/style.css';
+    link.media = 'all';
+    head.appendChild(link);
+};
+
+darkside.onclick = function() {
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = 'darkside';
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'css/darkside.css';
+    link.media = 'all';
+    head.appendChild(link);
 };
