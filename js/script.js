3b63a8111772856a4556bf26de2ee148459dd4d1
Nuno Azevedo <nunoemanuel1@gmail.com>
2018-05-14 19:29:32 +0100

    Typeface changed; New Theme; URL's fixed;  TextArea changed to read-only (#4)
    * Typeface changed; Add Dark Theme; URL's fixed;  TextArea changed to read-only
    
    * darktheme removed
    
    * Removed background-color

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var newPasswordButton = document.getElementById("newPassword");
 var generatedPasswordText = document.getElementById("generatedPassword");
 var numberOfWords = document.getElementById("numberOfWords");
 var separatorValue = document.getElementById("separatorValue");
+var light = document.getElementById("light");
+var darkside = document.getElementById("darkside");
+
 
 newPasswordButton.onclick = function() {
     let dicewareWords = document.querySelector('input[type="checkbox"]');
